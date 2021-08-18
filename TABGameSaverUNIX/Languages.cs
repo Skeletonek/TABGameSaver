e5db6368965063619e218d67000be01525a80e08
Łukasz Plich <skeleton0199@gmail.com>
2021-08-18 22:20:50 +0200

    Compiled for Unix systems

---
+++ TABGameSaverUNIX/Languages.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace TABGameSaver
+{
+    #region Copyright
+    // Copyright (C) 2019-2021 Skeletonek
+    //
+    // This program is licensed under GNU General Public License v3.0
+    // Full information in Program.cs file
+    #endregion
+    class Languages
+    {
+        public string[] strData;
+        CultureInfo ci;
+        public Languages()
+        {
+            ci = CultureInfo.InstalledUICulture;
+            switch (ci.TwoLetterISOLanguageName)
+            {
+                case "pl":
+                    LoadLanguage_PL();
+                    break;
+
+                case "en":
+                    LoadLanguage_EN();
+                    break;
+
+                default:
+                    LoadLanguage_EN();
+                    break;
+            }
+        }
+        private void LoadLanguage_PL()
+        {
+            strData = new string[]
+            {
+            "Proszę umieścić plik w katalogu z save'ami do gry They Are Billions\n(Dokumenty\\My Games\\They Are Billions\\Saves)\n", //0
+            "Aplikacja wykrywa tylko zapis z domyślną nazwą trybu survival.\nZmiana nazwy zapisu będzie skutkować nie działaniem aplikacji!\n",
+            "Czy aplikacja znajduje się w katalogu z zapisami do gry They Are Billions? (Y/N): ",
+            "!!! Nie można stworzyć katalogu aplikacji. Program nie będzie dalej kontynuował pracy !!!\n",
+            "Utworzono katalog aplikacji.",
+            "Sprawdzanie plików...", //5
+            "Wykryto plik",
+            "Próba wykonania kopii pliku...",
+            "\n!!! Nie można wykonać kopii pliku",
+            "Kopia pliku",
+            "wykonana pomyślnie", //10
+            "Wykryto zmiany w pliku",
+            "Próba wykonania kopii pliku...",
+            "Wykryto usunięcie pliku",
+            "Próba przywrócenia kopii pliku...",
+            "\n!!! Nie można przywrócić pliku", //15
+            "Przywrócenie pliku",
+            "wykonane pomyślnie.",
+            "Nie wykryto pliku",
+            "Brak kopii pliku.", //19
+            };
+
+        }
+        private void LoadLanguage_EN()
+        {
+            strData = new string[] {
+            "Please place this program inside of They Are Billions game saves directory\n(Documents\\My Games\\They Are Billions\\Saves)\n", //0
+            "Applications check for survival mode save only with default name.\nChanging save names may cause the aplication to not work properly!\n",
+            "Is this program placed inside They Are Billions game saves directory? (Y/N): ",
+            "!!! Cannot create app directory. Program will now stop working !!!\n",
+            "Created app directory.",
+            "Checking files...", //5
+            "Detected file",
+            "Attempting to make a copy of a file...",
+            "\n!!! Cannot create copy of a file",
+            "Copy of a file",
+            "created succesfully", //10
+            "Detected changes in file",
+            "Attempting to make a copy of a file...",
+            "Detected deletion of file",
+            "Attempting to restore a copy of the file...",
+            "\n!!! Cannot restore file", //15
+            "Restore of a file",
+            "completed succesfully.",
+            "Didn't detect file",
+            "No copying done.", //19
+            };
+        }
+    }
+}
---
+++ TABGameSaverUNIX/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace TABGameSaver
+{
+    #region Copyright
+    // Copyright (C) 2019-2021 Skeletonek
+    //
+    // This program is free software: you can redistribute it and/or modify
+    // it under the +terms of the GNU General Public License as published by
+    // the Free Software Foundation, either version 3 of the License, or
+    // (at your option) any later version.
+    //
+    // This program is distributed in the hope that it will be useful,
+    // but WITHOUT ANY WARRANTY; without even the implied warranty of
+    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    // GNU General Public License for more details.
+    //
+    // You should have received a copy of the GNU General Public License
+    // along with this program.  If not, see http://www.gnu.org/licenses/. 
+    #endregion
+    class Program
+    {
+        static string location = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        static Timer timer = new Timer(5000);
+        static string File1Location = Directory.GetCurrentDirectory() + @"\Survival.zxcheck";
+        static string File1BackupLocation = Directory.GetCurrentDirectory() + @"\TABGameSaverFiles\Survival.zxcheck";
+        static string File2Location = Directory.GetCurrentDirectory() + @"\Survival.zxsav";
+        static string File2BackupLocation = Directory.GetCurrentDirectory() + @"\TABGameSaverFiles\Survival.zxsav";
+        static string DirectoryLocation = Directory.GetCurrentDirectory() + @"\TABGameSaverFiles";
+
+        static Languages lang = new Languages();
+        public static void Main(string[] args)
+        {
+            timer.Elapsed += TimerElapsed;
+            timer.AutoReset = true;
+            Console.WriteLine("They Are Billions Game Saver\nVersion: 1.2.0\nAuthor: Skeletonek\nCopyright (C) 2019 - 2021\n\nThis program is licensed under GNU General Public License v3.0\n\n--------------------");
+            Console.WriteLine(lang.strData[0]);
+            Console.WriteLine(lang.strData[1]);
+            Console.WriteLine(lang.strData[2]);
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.WriteLine("\n\n");
+                timer.Enabled = true;
+                if (!Directory.Exists(DirectoryLocation))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(DirectoryLocation);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(location);
+                        Console.WriteLine(lang.strData[3]);
+                        timer.Enabled = false;
+                    }
+                    if (Directory.Exists(DirectoryLocation))
+                    {
+                        Console.WriteLine(lang.strData[4]);
+                    }
+                }
+                for (int i = 0; i >= 0; i++)
+                {
+                    if (Console.ReadKey().Key == ConsoleKey.Escape)
+                        return;
+                }
+            }
+        }
+
+        public static void TimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine(lang.strData[5]);
+            if (File.Exists(File1Location))
+            {
+                if (!File.Exists(File1BackupLocation))
+                {
+                    Console.WriteLine(lang.strData[6] + " \"Survival.zxcheck\". " + lang.strData[7]);
+                    try
+                    {
+                        File.Copy(File1Location, File1BackupLocation);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(lang.strData[8] + " \"Survival.zxcheck\" !!!\n");
+                    }
+                    if (File.Exists(File1BackupLocation))
+                    {
+                        Console.WriteLine(lang.strData[9] + " \"Survival.zxcheck\" " + lang.strData[10]);
+                    }
+                }
+                if (File.GetLastWriteTime(File1Location) != File.GetLastWriteTime(File1BackupLocation))
+                {
+                    Console.WriteLine(lang.strData[11] + " \"Survival.zxcheck\". " + lang.strData[12]);
+                    try
+                    {
+                        File.Delete(File1BackupLocation);
+                        File.Copy(File1Location, File1BackupLocation);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(lang.strData[8] + " \"Survival.zxcheck\" !!!\n");
+                    }
+                    if (File.Exists(File1BackupLocation))
+                    {
+                        Console.WriteLine(lang.strData[9] + " \"Survival.zxcheck\" " + lang.strData[10]);
+                    }
+                }
+            }
+            if (File.Exists(File2Location))
+            {
+                if (!File.Exists(File2BackupLocation))
+                {
+                    Console.WriteLine(lang.strData[6] + " \"Survival.zxsav\". " + lang.strData[7]);
+                    try
+                    {
+                        File.Copy(File2Location, File2BackupLocation);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(lang.strData[8] + " \"Survival.zxsav\" !!!\n");
+                    }
+                    if (File.Exists(File2BackupLocation))
+                    {
+                        Console.WriteLine(lang.strData[9] + " \"Survival.zxsav\" " + lang.strData[10]);
+                    }
+                }
+                if (File.GetLastWriteTime(File2Location) != File.GetLastWriteTime(File2BackupLocation))
+                {
+                    Console.WriteLine(lang.strData[11] + " \"Survival.zxsav\". " + lang.strData[12]);
+                    try
+                    {
+                        File.Delete(File2BackupLocation);
+                        File.Copy(File2Location, File2BackupLocation);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(lang.strData[8] + " \"Survival.zxsav\" !!!\n");
+                    }
+                    if (File.Exists(File2BackupLocation))
+                    {
+                        Console.WriteLine(lang.strData[9] + " \"Survival.zxsav\" " + lang.strData[10]);
+                    }
+                }
+            }
+            if (!File.Exists(File1Location))
+            {
+                if (File.Exists(File1BackupLocation))
+                {
+                    Console.WriteLine(lang.strData[13] + " \"Survival.zxcheck\". " + lang.strData[14]);
+                    try
+                    {
+                        File.Copy(File1BackupLocation, File1Location);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(lang.strData[15] + " \"Survival.zxcheck\" !!!\n");
+                    }
+                    if (File.Exists(File1Location))
+                    {
+                        Console.WriteLine(lang.strData[16] + " \"Survival.zxcheck\" " + lang.strData[17]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(lang.strData[18] + " \"Survival.zxcheck\". " + lang.strData[19]);
+                }
+            }
+            if (!File.Exists(File2Location))
+            {
+                if (File.Exists(File2BackupLocation))
+                {
+                    Console.WriteLine(lang.strData[13] + " \"Survival.zxsav\". " + lang.strData[14]);
+                    try
+                    {
+                        File.Copy(File2BackupLocation, File2Location);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(lang.strData[15] + " \"Survival.zxsav\" !!!\n");
+                    }
+                    if (File.Exists(File2Location))
+                    {
+                        Console.WriteLine(lang.strData[16] + " \"Survival.zxsav\" " + lang.strData[17]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(lang.strData[18] + " \"Survival.zxsav\". " + lang.strData[19]);
+                }
+            }
+        }
+    }
+}
